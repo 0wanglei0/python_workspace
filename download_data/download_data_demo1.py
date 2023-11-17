48c6320d1fd84431716f8f3c76a0e85e6ede3c68
wang_lei <845934759@qq.com>
2023-11-17 16:37:57 +0800

    Update download_data_demo1.py
    add title
    add fill

--- download_data/download_data_demo1.py
+++ download_data/download_data_demo1.py
@@ -12,17 +12,28 @@ with open(filename, "r", encoding="utf-8") as f:
 
     dates, close, open_price = [], [], []
     for row in reader:
-        close.append(float(row[2]))
-        dates.append(row[0])
-        open_price.append(float(row[1]))
+        try:
+            current_date = row[0]
+            current_close = float(row[2])
+            current_open_price = float(row[1])
+        except ValueError:
+            print('missing data')
+        else:
+            dates.append(current_date)
+            close.append(current_close)
+            open_price.append(current_open_price)
 
-print(open_price)
+# print(open_price)
+
 fig = plt.figure(dpi=128, figsize=(60, 6))
-plt.plot(dates, close, c='red')
-plt.plot(dates, open_price, c='blue')
+plt.plot(dates, close, c='red', alpha=0.5)
+plt.plot(dates, open_price, c='blue', alpha=0.5)
+plt.fill_between(dates, open_price, close, facecolor="blue", alpha=0.1)  # 填充两条线中间区域
 plt.xlabel("date", fontsize=16)
 plt.ylabel("price", fontsize=16)
 fig.autofmt_xdate()
 plt.gcf().autofmt_xdate()
 plt.tick_params(axis='both', which="major", labelsize=15)
+title = "A data"
+plt.title(title, fontsize=20)
 plt.show()
